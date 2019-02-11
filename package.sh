b3ac39f21101f31e9baee7b96d1da2cc1bdeafc9
ShellingfordX <34960057+ShellingfordX@users.noreply.github.com>
2019-02-12 00:37:12 +0900

    Update package.sh

--- package.sh
+++ package.sh
@@ -15,11 +15,10 @@ cp ../*.png ${ARCHIVEDIR}
 cp ../*.sh ${ARCHIVEDIR}
 cp ../README* ${ARCHIVEDIR}
 cp ../LICENSE* ${ARCHIVEDIR}
-cp ../INSTALL* ${ARCHIVEDIR}
 cp -r ../dialog/ ${ARCHIVEDIR}
 
 rm -f ${ARCHIVEDIR}/package.sh
 
-tar cvzf dsaber-plymouth-theme_v${VERSION}.tar.gz ${ARCHIVEDIR}/
+tar cvzf saber-plymouth-theme_v${VERSION}.tar.gz ${ARCHIVEDIR}/
 
 rm -rf ${ARCHIVEDIR}
